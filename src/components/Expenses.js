8925f4438f68832c0eabe8929e981d9bde44f636
Matthew <mdh0588@gmail.com>
2023-08-16 19:51:46 -0700

    changing port for website

--- src/components/Expenses.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import api from "../api/api";
-
-const Expenses = () => {
-  
-};
-
-export default Expenses;
--- src/features/api/api.js
+++ src/features/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5050', // Your Express server's URL
+  baseURL: 'http://localhost:10000', // Your Express server's URL
   withCredentials: true, // Set withCredentials to true
 });
 
