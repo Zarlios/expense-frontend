a00dfb88f53a975da444aea3de4c1d41b5733e34
Matthew <mdh0588@gmail.com>
2023-08-16 20:09:05 -0700

    adding port

--- src/features/api/api.js
+++ src/features/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://expense-backend-7vd9.onrender.com',
+  baseURL: 'https://expense-backend-7vd9.onrender.com:10000',
 
   withCredentials: true, // Set withCredentials to true
 });
