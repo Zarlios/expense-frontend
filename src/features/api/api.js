342b63f0f2cc6fada460e2eb0c7f56dfe8af827a
Matthew <mdh0588@gmail.com>
2023-08-16 20:17:50 -0700

    trying without port

--- src/features/api/api.js
+++ src/features/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://expense-backend-7vd9.onrender.com:10000',
+  baseURL: 'https://expense-backend-7vd9.onrender.com',
 
   withCredentials: true, // Set withCredentials to true
 });
